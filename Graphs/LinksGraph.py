31e836c5405115a9890a31aaeccb6379f89478e5
NowSayMyName <yoann.rouxel@insa-lyon.fr>
2020-11-27 11:48:37 +0100

    added init

---
+++ Graphs/LinksGraph.py
@@ -0,0 +1,10 @@
+Class LinksGraph
+
+	def __init__(self, RouteurGraph): 
+        self.routeurs = RouteurGraph
+        self.graph = [[0 for column in range(RouteurGraph)]  
+                    for row in range(RouteurGraph)] 
+
+	def
+			
+
