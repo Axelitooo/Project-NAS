3fbc04e90c4ef3770e4f5b8c472b2cf6d149fd29
NowSayMyName <yoann.rouxel@insa-lyon.fr>
2020-11-27 12:00:52 +0100

    tried dijkstra

--- Graphs/LinksGraph.py
+++ Graphs/LinksGraph.py
@@ -1,10 +1,16 @@
 Class LinksGraph
 
 	def __init__(self, RouteurGraph): 
-        self.routeurs = RouteurGraph
+        self.routers = RouteurGraph
         self.graph = [[0 for column in range(RouteurGraph)]  
                     for row in range(RouteurGraph)] 
 
-	def
+	""" """
+	def dijkstra(self, source_router):
+		dist = [self.routers] * [self.routers]
+		for 
+
+		dist[source_router] = 0
+
 			
 
