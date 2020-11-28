b052d3e215cf2e1702cb2d754bbdd2545e9333b9
rdolgopol <rdologpol@github.com>
2020-11-28 21:51:50 +0200

    Djikstra algorithm

---
+++ Graphs/RouteurGraph.py
@@ -0,0 +1,52 @@
+import sys
+import networkx as nx
+
+def shortestpath(graph,start,end,visited=[],distances={},predecessors={}):
+    """Find the shortest path between start and end nodes in a graph"""
+    # we've found our end node, now find the path to it, and return
+    if start==end:
+        path=[]
+        while end != None:
+            path.append(end)
+            end=predecessors.get(end,None)
+        return distances[start], path[::-1]
+    # detect if it's the first time through, set current distance to zero
+    if not visited: distances[start]=0
+    # process neighbors as per algorithm, keep track of predecessors
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            neighbordist = distances.get(neighbor,sys.maxsize)
+            tentativedist = distances[start] + graph[start][neighbor]
+            if tentativedist < neighbordist:
+                distances[neighbor] = tentativedist
+                predecessors[neighbor]=start
+    # neighbors processed, now mark the current node as visited
+    visited.append(start)
+    # finds the closest unvisited node to the start
+    unvisiteds = dict((k, distances.get(k,sys.maxsize)) for k in graph if k not in visited)
+    closestnode = min(unvisiteds, key=unvisiteds.get)
+    # now we can take the closest node and recurse, making it current
+    return shortestpath(graph,closestnode,end,visited,distances,predecessors)
+
+if __name__ == "__main__":
+    graph = {'a': {'w': 14, 'x': 7, 'y': 9},
+            'b': {'w': 9, 'z': 6},
+            'w': {'a': 14, 'b': 9, 'y': 2},
+            'x': {'a': 7, 'y': 10, 'z': 15},
+            'y': {'a': 9, 'w': 2, 'x': 10, 'z': 11},
+            'z': {'b': 6, 'x': 15, 'y': 11}}
+    print(shortestpath(graph,'a','b'))
+
+    """
+    Result:
+        (20, ['a', 'y', 'w', 'b'])
+        
+    G = nx.DiGraph()
+    G.add_edge('a', 'w' , weight=7, capacity=15,
+               G.add_edgefrom('a', '', weight=4)
+    G.add_edge('B', 'D', weight=2)
+    G.add_edge('A', 'C', weight=3)
+    G.add_edge('C', 'D', weight=4)
+    print(nx.shortest_path(G, 'A', 'D', weight='weight'))
+
+    """
