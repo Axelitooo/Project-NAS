1e76b66111277e45bf0c45b370cc6947648407f3
nyandog26 <48133528+nyandog26@users.noreply.github.com>
2020-12-07 12:00:06 +0100

    Router and packet class + update of calendar queue
    The calendar queue must be in simulated time, and the router need qequence number in LSDB

--- calendarQueue.py
+++ calendarQueue.py
@@ -1,15 +1,30 @@
 import sched, time
+from Token_bucket.storage import MemoryStorage
+from .router import Router
+
+
 s = sched.scheduler(time.time, time.sleep)
+"""
+a = Router()
+b = Router()
+x = Router()
+y = Router()
+z = Router()
+"""
+listRouter = [a, b, x, y, z]
+listBucket = MemoryStorage()
 
-def sendPacket(num, type, propagation):
+def sendPacket(type, propagation, source, destination): #add a sending packet to a neighbour in the calendarQueue
     print("packet send num: " , num)
-    s.enter(propagation,1, receivePacket, argument=(num, type))
+    s.enter(propagation,"LSP", receivePacket, argument=(num, type))
+    source.sendPacket()
 
-def receivePacket(num, type):
+def receivePacket(num, type, destination):
     print("packet received num: " , num)
+    destination.receivePacket()
 
-s.enter(5,1, sendPacket, argument=(1, "LSP",2))
-s.enter(5,2, sendPacket, argument=(2, "LSP",5))
+s.enter(5,1, sendPacket, argument=("LSP",2, a.id, b.id))
+s.enter(5,2, sendPacket, argument=("LSP",5, a.id, c.id))
 #s.enter(10,1, receivePacket, argument=(1))
 #s.enter(10,2, receivePacket, argument=(2))
 
---
+++ router.py
@@ -0,0 +1,60 @@
+class Router:
+
+    def __init__(self,id , state, tokenBucket, neighbours, LSDB):
+        self.__id = id
+        self.__state = state                # int or boolean
+        self.__tokenBucket = tokenBucket    # id of tockenBucket
+        self.__neighbours = neighbours      # dictionnary
+        self.__LSDB = LSDB                  # list
+
+    def showNeighbours():
+        print(self.__neighbours)
+
+    def addNeighbours(id, weight):
+        if id not in self.__neighbours.keys():
+            self.__neighbours[id] = weight
+        else:
+            print("Id already in dictionnary")
+        
+    def computeShortestPath(router):
+        print("computing shortest path")
+
+    def receivePacket(packet):
+        if state != 0: # if !down
+            if packet.packetType == "ACK":
+                expectedAcks.pop(packet.destination)
+                print("ACK received by " + self.id + " from " + packet.source)
+            else if packet.packetType == "LSP":
+                ack = Packet(source = packet.destination, destination = packet.source, packetType = "ACK")
+                sendPacket(ack)
+                for router in self.neighbors.keys():
+                    if router != packet.source:
+                        retransmitPacket = Packet(source = self.id, destination = router, packetType = "LSP", content = packet.content)
+                        sendPacket(retransmitPacket)
+                print("LSP received by " + self.id + " from " + packet.source)
+        
+    def sendPacket(packet):
+        if packet.packetType == "ACK":
+            print("ACK sent by " + self.id + " to " + packet.destination)
+        else if packet.packetType == "LSP":
+            expectedAcks[packet.destination] = TIME # TO DEFINE
+            print("LSP sent by " + self.id + " to " + packet.destination)
+            
+    def usefulContent(packet):
+        for element in packet.content:
+            if element not in LSBD:
+                return True
+        return False
+    
+class Packet(source, destination, packetType, content):
+    def __init__(self):
+        self.source = source # router ID
+        self.destination = destination # router ID
+        self.packetType = packetType # packetType is a string, "ACK" or "LSP"
+        self.content = content # content is a dictionary of keys = links ("id1-id2"), values = weights
+
+
+        
+
+    
+    
