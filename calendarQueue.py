7b6750c608432f7e9918f38ec97f4c8ecfafc213
rdolgopol <romina.dolgopol@gmail.com>
2021-01-10 22:53:14 +0200

    moved seqnum from packet to db entry

--- calendarQueue.py
+++ calendarQueue.py
@@ -3,27 +3,29 @@ from storage import MemoryStorage
 from token_bucket import TokenBucket
 from router import Router, Packet
 
-class CalendarQueue:
-    def __init__(self, timefunc, delayfunc):
-        self.scheduler = sched.scheduler(timefunc, delayfunc)
 
 def demi(t):
     return
 
-def sendPacket(self, propagation, router_source,
-               router_destination, packet):  # add a sending packet to a neighbour in the calendarQueue
-    if packet is None:
-        packet = Packet(router_source, router_destination, 0, "LSP", None)
-    print("packet send to : ", router_destination)
-    listRouter[router_source].send_packet_now(packet, time.time_ns())
-    s.enter(propagation, 1, receivePacket, argument=(packet, router_destination))
 
+class CalendarQueue:
+    def __init__(self, timefunc, delayfunc):
+        self.scheduler = sched.scheduler(timefunc, delayfunc)
 
-def receivePacket(self, packet, router_destination):
-    res = listRouter[router_destination].receive_packet(packet)
-    print("packet received by :", packet.destination, "return code :", res)
-    if res == 1:
-        listRouter[router_destination].process_packet()
+    def sendPacket(self, propagation, router_source,
+                   router_destination, packet):  # add a sending packet to a neighbour in the calendarQueue
+        print(listRouter[router_source].LSDB)
+        if packet is None:
+            packet = Packet(router_source, router_destination, 0, "LSP", None)
+        print("packet send to : ", router_destination)
+        listRouter[router_source].send_packet_now(packet, time.time_ns())
+        self.scheduler.enter(propagation, 1, self.receivePacket, argument=(packet, router_destination))
+
+    def receivePacket(self, packet, router_destination):
+        res = listRouter[router_destination].receive_packet(packet)
+        print("packet received by :", packet.destination, "return code :", res)
+        if res == 1:
+            listRouter[router_destination].process_packet()
 
 
 storage_a = MemoryStorage()
@@ -43,19 +45,27 @@ bucket_z = TokenBucket(rate, capacity, storage_z)
 
 calendarQueue = CalendarQueue(timefunc=time.time_ns, delayfunc=demi)
 
-a = Router(id=0, state=True, tokenBucket=bucket_a, neighbours={}, LSDB=[], bufferSize=10, sendfunc=sendPacket, linkStates=None)
-b = Router(id=1, state=True, tokenBucket=bucket_b, neighbours={0:a}, LSDB=[], bufferSize=10, sendfunc=sendPacket, linkStates=None)
-x = Router(id=2, state=True, tokenBucket=bucket_x, neighbours={0:a, 1:b}, LSDB=[], bufferSize=10, sendfunc=sendPacket, linkStates=None)
-y = Router(id=3, state=True, tokenBucket=bucket_y, neighbours={}, LSDB=[], bufferSize=10, sendfunc=sendPacket, linkStates=None)
-z = Router(id=4, state=True, tokenBucket=bucket_z, neighbours={}, LSDB=[], bufferSize=10, sendfunc=sendPacket, linkStates=None)
+a = Router(id=0, state=True, tokenBucket=bucket_a, neighbours={}, LSDB={}, bufferSize=10, calendar=calendarQueue,
+           linkStates=None)
+b = Router(id=1, state=True, tokenBucket=bucket_b, neighbours={}, LSDB={}, bufferSize=10, calendar=calendarQueue,
+           linkStates=None)
+x = Router(id=2, state=True, tokenBucket=bucket_x, neighbours={}, LSDB={}, bufferSize=10, calendar=calendarQueue,
+           linkStates=None)
+y = Router(id=3, state=True, tokenBucket=bucket_y, neighbours={}, LSDB={}, bufferSize=10, calendar=calendarQueue,
+           linkStates=None)
+z = Router(id=4, state=True, tokenBucket=bucket_z, neighbours={}, LSDB={}, bufferSize=10, calendar=calendarQueue,
+           linkStates=None)
 
 listRouter = [a, b, x, y, z]
 a.add_neighbour(1, 1)
 a.add_neighbour(2, 1)
+b.add_neighbour(0, 1)
 b.add_neighbour(2, 1)
+x.add_neighbour(0, 1)
+x.add_neighbour(1, 1)
 
-calendarQueue.scheduler.enter(0, 1, sendPacket, argument=(2000000, 0, 2, None))  # , a.id, b.id
-#s.enter(0, 2, sendPacket, argument=(5000000, 2, 3, None))
+calendarQueue.scheduler.enter(0, 1, calendarQueue.sendPacket, argument=(2000000, 0, 2, None))  # , a.id, b.id
+# s.enter(0, 2, sendPacket, argument=(5000000, 2, 3, None))
 # s.enter(10,1, receivePacket, argument=(1))
 # s.enter(10,2, receivePacket, argument=(2))
 
--- router.py
+++ router.py
@@ -1,57 +1,45 @@
 import queue
+import sys
 from token_bucket import TokenBucket
 from storage import MemoryStorage
 
 
 class Router:
 
-    def __init__(self, id, state, tokenBucket, neighbours, linkStates, LSDB, bufferSize, sendfunc):
+    def __init__(self, id, state, tokenBucket, neighbours, linkStates, LSDB, bufferSize, calendar):
         self.id = id
         self.state = state  # int or boolean
         self.tokenBucket = tokenBucket  # list of tokenBucket
         self.neighbours = neighbours  # dictionary
         self.LSDB = LSDB  # list
-        self.lastLSP = -1
         self.buffer = queue.Queue(bufferSize)
-        self.sendfunc = sendfunc
-        self.expectedAcks = {} #dictionary of packet to be ACKed
+        self.calendar = calendar
+        self.expectedAcks = {}  # dictionary of packet to be ACKed
 
     def show_neighbours(self):
         print(self.neighbours)
 
+    def update_lsdb(self, link, value):
+        for key in self.LSDB.keys():
+            if key == link:
+                if value["weight"] == -1:
+                    self.LSDB.remove(link)
+                    return True
+                elif self.LSDB[link]["weight"] != value["weight"]:
+                    self.LSDB[link] = value
+                    return True
+                else:
+                    return False
+        # if reached here means link not in LSDB
+        self.LSDB[link] = value
+        return True
+
     def add_neighbour(self, id, weight):
         if id not in self.neighbours.keys():
             self.neighbours[id] = weight
+            self.update_lsdb(str(self.id) + "->" + str(id), {"weight": weight, "seqnum": 1})
         else:
             print("Id already in dictionary")
-
-    def compute_shortest_path(self, start,end,visited=[],distances={},predecessors={}):
-        print("computing shortest path")
-        """Find the shortest path between start and end nodes in a graph (LSDB)"""
-        # we've found our end node, now find the path to it, and return
-        if start==end:
-            path=[]
-            while end != None:
-                path.append(end)
-                end=predecessors.get(end,None)
-            return distances[start], path[::-1]
-        # detect if it's the first time through, set current distance to zero
-        if not visited: distances[start]=0
-        # process neighbors as per algorithm, keep track of predecessors
-        for neighbor in self.LSDB[start]:
-            if neighbor not in visited:
-                neighbordist = distances.get(neighbor,sys.maxsize)
-                tentativedist = distances[start] + self.LSDB[start][neighbor]
-                if tentativedist < neighbordist:
-                    distances[neighbor] = tentativedist
-                    predecessors[neighbor]=start
-        # neighbors processed, now mark the current node as visited
-        visited.append(start)
-        # finds the closest unvisited node to the start
-        unvisiteds = dict((k, distances.get(k,sys.maxsize)) for k in self.LSDB if k not in visited)
-        closestnode = min(unvisiteds, key=unvisiteds.get)
-        # now we can take the closest node and recurse, making it current
-        return shortestpath(self, closestnode,end,visited,distances,predecessors)
 
     def process_packet(self):
         print("processing packet")
@@ -59,36 +47,66 @@ class Router:
         if self.state:  # if !down
             if packet.packetType == "ACK":
                 # TODO queue.cancel(packet.source, seqnum)
-                expectedAcks.pop(packet.seqnum)
+                self.expectedAcks.pop(packet.seqnum)
                 print("ACK received by " + str(self.id) + " from " + str(packet.source))
             elif packet.packetType == "LSP":
                 print("processing LSP")
                 print("LSP received by " + str(self.id) + " from " + str(packet.source))
-                ack = Packet(source=packet.destination, destination=packet.source, packetType="ACK", seqnum=packet.seqnum, content=None)
+                ack = Packet(source=packet.destination, destination=packet.source, packetType="ACK",
+                             content=None, id=packet.id)
                 # every LSP packet is ACKed; similarly to TCP behaviour rather than OSPF's one
                 # TODO ACK only if update + cancel if updating LSP received
                 self.send_packet(ack)
-                if self.lastLSP != packet.seqnum:
-                    if self.lastLSP < packet.seqnum:
-                        print("Router", self.id, "updating LSDB")
-                        # update your own LSDB
-                        self.LSDB = packet.content
-                        self.lastLSP = packet.seqnum
-                        # flood
-                        print("Router", self.id, "FLOODING!!")
-                        for router in self.neighbours.keys():
-                            if router != packet.source:
-                                retransmit_packet = Packet(source=self.id, destination=router, packetType="LSP",
-                                                           content=self.LSDB, seqnum=self.lastLSP)
-                                self.send_packet(retransmit_packet)
-                                # handle somehow the case when the LSP was lost (ACK not received), related to expectedAcks
-                    else:
-                        print("Router", self.id, "updating", packet.source, "with seqnum", self.lastLSP)
-                        # the source (other router) has an outdated information, we should update him
-                        retransmit_packet = Packet(source=self.id, destination=packet.source, packetType="LSP",
-                                                   content=self.LSDB, seqnum=self.lastLSP)
-                        self.send_packet(retransmit_packet)
-                        # handle somehow the case when the LSP was lost (ACK not received), related to expectedAcks
+                for link in packet.content.keys():
+                    if self.lastLSP != packet.seqnum:
+                        if self.lastLSP < packet.seqnum:
+                            print("Router", self.id, "updating LSDB")
+                            # update your own LSDB
+                            self.LSDB = packet.content
+                            self.lastLSP = packet.seqnum
+                            # flood
+                            print("Router", self.id, "FLOODING!!")
+                            for router in self.neighbours.keys():
+                                if router != packet.source:
+                                    retransmit_packet = Packet(source=self.id, destination=router, packetType="LSP",
+                                                               content=self.LSDB, seqnum=self.lastLSP)
+                                    self.send_packet(retransmit_packet)
+                                    # handle somehow the case when the LSP was lost (ACK not received), related to expectedAcks
+                        else:
+                            print("Router", self.id, "updating", packet.source, "with seqnum", self.lastLSP)
+                            # the source (other router) has an outdated information, we should update him
+                            retransmit_packet = Packet(source=self.id, destination=packet.source, packetType="LSP",
+                                                       content=self.LSDB, seqnum=self.lastLSP)
+                            self.send_packet(retransmit_packet)
+                            # handle somehow the case when the LSP was lost (ACK not received), related to expectedAcks
+
+    def compute_shortest_path(self, start, end, visited=[], distances={}, predecessors={}):
+        print("computing shortest path")
+        """Find the shortest path between start and end nodes in a graph (LSDB)"""
+        # we've found our end node, now find the path to it, and return
+        if start == end:
+            path = []
+            while end is not None:
+                path.append(end)
+                end = predecessors.get(end, None)
+            return distances[start], path[::-1]
+        # detect if it's the first time through, set current distance to zero
+        if not visited: distances[start] = 0
+        # process neighbors as per algorithm, keep track of predecessors
+        for neighbor in self.LSDB[start]:
+            if neighbor not in visited:
+                neighbordist = distances.get(neighbor, sys.maxsize)
+                tentativedist = distances[start] + self.LSDB[start][neighbor]
+                if tentativedist < neighbordist:
+                    distances[neighbor] = tentativedist
+                    predecessors[neighbor] = start
+        # neighbors processed, now mark the current node as visited
+        visited.append(start)
+        # finds the closest unvisited node to the start
+        unvisiteds = dict((k, distances.get(k, sys.maxsize)) for k in self.LSDB if k not in visited)
+        closestnode = min(unvisiteds, key=unvisiteds.get)
+        # now we can take the closest node and recurse, making it current
+        return self.compute_shortest_path(closestnode, end, visited, distances, predecessors)
 
     def receive_packet(self, packet):
         if self.buffer.full():
@@ -100,7 +118,7 @@ class Router:
     # sends packet to the scheduler
     def send_packet(self, packet):
         delay = 1_000_000_000
-        self.sendfunc(delay, packet.source, packet.destination, packet)
+        self.calendar.sendPacket(delay, packet.source, packet.destination, packet)
 
     def send_packet_now(self, packet, now):
         # if the destination is considered to be busy the packed is dropped on the source
@@ -108,22 +126,22 @@ class Router:
             if packet.packetType == "ACK":
                 print("ACK sent by " + str(self.id) + " to " + str(packet.destination))
             elif packet.packetType == "LSP":
-                self.expectedAcks[packet.seqnum] = packet
+                self.expectedAcks[packet.id] = packet
                 # TODO add an event to the calender queue saying "if the router hasn't received an ACK by this time, retransmit the packet"
                 print("LSP sent by " + str(self.id) + " to " + str(packet.destination))
 
     def useful_content(self, packet):
-        for element in packet.content:
+        for element in packet.content.keys():
             if element not in self.LSDB:
                 return True
         return False
 
 
 class Packet:
-    def __init__(self, source, destination, seqnum, packetType, content):
+    def __init__(self, source, destination, packetType, content, id):
         self.source = source  # router ID
         self.destination = destination  # router ID
-        self.seqnum = seqnum
         self.packetType = packetType  # packetType is a string, "ACK" or "LSP"
         self.content = content  # content is a dictionary of keys = links ("id1-id2"), values = weights
+        self.id = id # id of the packet, used for ACK
         self.size = 1  # ACK and LSP occupies the same buffer size
